54775bf28a37dfde61d461443584f98a7d907593
Chemi <josemiguel.guerrerojim@gmail.com>
2025-11-05 22:25:18 +0100

    Implementation of swiftui tutorial of composing complex interfaces in order to create the categories components with the detail view

---
+++ Musinepolis/Frontend/Components/Category/CategoryView.swift
@@ -0,0 +1,37 @@
+//
+//  CategoryView.swift
+//  Musinepolis
+//
+//  Created by José Miguel Guerrero Jiménez on 05/11/25.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    @Environment(ModelData.self) var modelData
+    var body: some View {
+        NavigationSplitView{
+            List {
+                modelData.features[0].image
+                                    .resizable()
+                                    .scaledToFill()
+                                    .frame(height: 200)
+                                    .clipped()
+                                    .listRowInsets(EdgeInsets())
+                            ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
+                                CategoryRow(categoryName: key, items: modelData.categories[key]!)
+
+                            }
+                            .listRowInsets(EdgeInsets())
+                        }
+                .navigationTitle("Movies")
+        } detail: {
+            Text("Select a landmark")
+        }
+    }
+}
+
+#Preview {
+    CategoryView()
+        .environment(ModelData())
+}
